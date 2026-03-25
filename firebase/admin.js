143f4f518e87e56ae75c7a3b4ec27b6c0a6f274a
BAUJI4 <baujisensi54@gmail.com>
2026-03-25 19:29:09 +0530

    Update admin.js

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,11 +1,23 @@
 const admin = require("firebase-admin");
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_KEY || "{}");
+let adminInstance;
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+try {
+  if (!admin.apps.length) {
+    if (!process.env.FIREBASE_KEY) {
+      throw new Error("FIREBASE_KEY missing");
+    }
+
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+    adminInstance = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } else {
+    adminInstance = admin.app();
+  }
+} catch (error) {
+  console.error("Firebase Init Error:", error.message);
 }
 
 module.exports = admin;
